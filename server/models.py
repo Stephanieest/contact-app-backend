1d28eb80c83fec92ed1785ba392eb3e8e8a09ae1
Stephanie <stephnjonjo@gmail.com>
2025-01-28 21:04:28 +0300

    dupdated my user and contact model

--- server/models.py
+++ server/models.py
@@ -4,16 +4,17 @@ db = SQLAlchemy()
 
 class Use(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     contacts = db.relationship('Contact', backref='user', lazy=True)
     favourites = db.relationship('Favourite', backref-'user', lazy=True)
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(255),unique=True , nullable=False)
+    address = db.Column(db.Text, nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def to_dict(self):
@@ -21,6 +22,8 @@ class Contact(db.Model):
             'id': self.id,
             'name': self.name,
             'phone': self.phone,
-            'address_id': self.address_id,
+            'email': self.email,
+            'address': self.address,
             'user_id': self.user_id
         }
+    
