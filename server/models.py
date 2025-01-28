fadcd72819cb9bae0abc3893ce2ccac8a9323e84
Stephanie <stephnjonjo@gmail.com>
2025-01-28 13:17:48 +0300

    defined the contact model

--- server/models.py
+++ server/models.py
@@ -8,3 +8,19 @@ class Use(db.Model):
     contacts = db.relationship('Contact', backref='user', lazy=True)
     favourites = db.relationship('Favourite', backref-'user', lazy=True)
 
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'phone': self.phone,
+            'address_id': self.address_id,
+            'user_id': self.user_id
+        }
