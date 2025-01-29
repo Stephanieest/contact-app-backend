b6b708612743cdfb1d525cdfd12330e8fc4ba1b1
Stephanie <stephnjonjo@gmail.com>
2025-01-29 09:47:42 +0300

    created the contact route

--- server/routes.py
+++ server/routes.py
@@ -17,3 +17,20 @@ def init_app(app):
         users = User.query.all()
         return jsonify([{"id": user.id, "username": user.username, "email": user.email} for user in users]), 200
     
+    @app.route('api/contacts', methods=['GET', 'POST'])
+    def handle_contacts():
+        if request.method == 'POST':
+            data = request.get_json()
+            contact = Contact(
+                name=data['name'],
+                phone=data['phone'],
+                email=data['email'],
+                address=data['address'],
+                user_id=data['user_id'],
+            )
+            db.session.add(contact)
+            db.session.commit()
+            return jsonify({"message": "Contact created", "contact": contact.to_dict()}), 201
+        
+        contacts = Contact.query.all()
+        return jsonify([contact.to_dict() for contact in contacts]), 200
